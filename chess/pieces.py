3cd9c35c9e712c43676398cb8e97f1939fa662f8
trewjames <j.trew10@gmail.com>
2020-08-18 11:54:36 -0400

    Refactor appends in get_moves.

--- chess/pieces.py
+++ chess/pieces.py
@@ -41,18 +41,36 @@ class Piece:
         if not isinstance(piece, Null):
             return piece.is_white != self.is_white
 
-    def _check_move(new_r, new_c):
-        """ [Helper function] - currently unused.
-            Add move. Returns False if a capture is possible.
-            Return False causes a break.
-        """
-        if isinstance(game[(new_r, new_c)], Null):
-            moves.append((new_r, new_c))
-            return True
+    def _range_append(self, dx, dy, game):
+        """ Appends move to moves for pieces with inf range """
+        multi = 1
+        while True:
+            new_r = self.row + multi * dx
+            new_c = self.col + multi * dy
+            sqr = (new_r, new_c)
+
+            if new_r < 0 or new_r >= DIM or new_c < 0 or new_c >= DIM:
+                break
+            if isinstance(game[sqr], Null):
+                self.moves.append(sqr)
+            elif self._check_capture(new_r, new_c, game):
+                self.moves.append(sqr)
+                break
+            else:
+                break
+            multi += 1
+
+    def _close_append(self, dx, dy, game):
+        new_r = self.row + dx
+        new_c = self.col + dy
+        sqr = (new_r, new_c)
+
+        if new_r < 0 or new_r >= DIM or new_c < 0 or new_c >= DIM:
+            return
+        if isinstance(game[sqr], Null):
+            self.moves.append(sqr)
         elif self._check_capture(new_r, new_c, game):
-            moves.append((new_r, new_c))
-            return False
-        return False
+            self.moves.append(sqr)
 
 
 class Pawn(Piece):
@@ -88,82 +106,35 @@ class Pawn(Piece):
 class Rook(Piece):
 
     def get_moves(self, game):
-        moves = []
-
-        def append(dx, dy):
-            multi = 1
-            while True:
-                new_r = self.row + multi * dx
-                new_c = self.col + multi * dy
-                sqr = (new_r, new_c)
-
-                if new_r < 0 or new_r >= DIM or new_c < 0 or new_c >= DIM:
-                    break
-                if isinstance(game[sqr], Null):
-                    moves.append(sqr)
-                elif self._check_capture(new_r, new_c, game):
-                    moves.append(sqr)
-                    break
-                else:
-                    break
-                multi += 1
+        self.moves = []
 
         for dx, dy in permutations([1, -1, 0], 2):
             if abs(dx) != abs(dy):
-                append(dx, dy)
-        return moves
+                self._range_append(dx, dy, game)
+        return self.moves
 
 
 class Bishop(Piece):
 
     def get_moves(self, game):
-        moves = []
-
-        def append(dx, dy):
-            multi = 1
-            while True:
-                new_r = self.row + multi * dx
-                new_c = self.col + multi * dy
-                sqr = (new_r, new_c)
-
-                if new_r < 0 or new_r >= DIM or new_c < 0 or new_c >= DIM:
-                    break
-                if isinstance(game[sqr], Null):
-                    moves.append(sqr)
-                elif self._check_capture(new_r, new_c, game):
-                    moves.append(sqr)
-                    break
-                else:
-                    break
-                multi += 1
+        self.moves = []
 
         for dx in (-1, 1):
             for dy in (-1, 1):
-                append(dx, dy)
-        return moves
+                self._range_append(dx, dy, game)
+        return self.moves
 
 
 class Night(Piece):
     """ Night used in place of Knight """
 
     def get_moves(self, game):
-        moves = []
+        self.moves = []
 
         for dx, dy in permutations([1, 2, -1, -2], 2):
-            if abs(dx) == abs(dy):
-                continue
-
-            new_r = self.row + dx
-            new_c = self.col + dy
-            sqr = (new_r, new_c)
-
-            if new_r < 0 or new_r >= DIM or new_c < 0 or new_c >= DIM:
-                continue
-            if isinstance(game[sqr], Null):
-                moves.append(sqr)
-            elif self._check_capture(new_r, new_c, game):
-                moves.append(sqr)
-        return moves
+            if abs(dx) != abs(dy):
+                self._close_append(dx, dy, game)
+        return self.moves
 
 
 class King(Piece):
