41dddf638077ee80bc84e2bdffe56be3e5f4b78f
trewjames <j.trew10@gmail.com>
2020-08-18 21:44:42 -0400

    Adjust get_moves test.

--- tests/use_cases/test_GetMoves.py
+++ tests/use_cases/test_GetMoves.py
@@ -1,11 +1,11 @@
-from chess.engine import GetMoves
+from chess.engine import get_all_moves
 from chess.board import Board
 
 
 def test_start_get_all_moves():
     # bad incomplete
-    board = Board(player_white=True, white_to_move=True)
-    moves = GetMoves().get_all_moves(board)
+    board = Board()
+    moves = get_all_moves(board)
 
     assert moves == set([(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6),
                          (2, 7), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5),
