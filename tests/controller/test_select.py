ea19f702808cc8581a9324b35ba99ff24d4ed051
trewjames <j.trew10@gmail.com>
2020-08-21 11:15:20 -0400

    Parametrize and refactor select test (test assert Move init).

--- tests/controller/test_select.py
+++ tests/controller/test_select.py
@@ -1,23 +1,8 @@
 from chess.controller import Select
 from chess.board import Board
-from chess import engine
 
 import pytest
-from unittest import mock
-
-
-@pytest.fixture
-def game_grid():
-    return [
-        ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
-        ["bp", "bp", "bp", "bp", "--", "bp", "bp", "bp"],
-        ["--", "--", "--", "--", "--", "--", "--", "--"],
-        ["--", "--", "--", "--", "bp", "--", "--", "--"],
-        ["--", "--", "--", "wp", "--", "--", "--", "--"],
-        ["--", "--", "--", "--", "--", "--", "--", "--"],
-        ["wp", "wp", "wp", "--", "wp", "wp", "wp", "wp"],
-        ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]
-    ]
+from unittest.mock import patch
 
 
 def test_select_init():
@@ -27,102 +12,82 @@ def test_select_init():
     assert select.moves == []
 
 
-def test_select_first_valid():
-    game = Board()
+# --- First Selection --- # noqa
+@pytest.mark.parametrize(
+    "sel1, pos1, result", [
+        ((6, 3), (6, 3), [(5, 3), (4, 3)]),  # valid selection
+        ((1, 0), None, []),                  # invalid, wrong turn
+        ((3, 3), None, [])                   # invalid, empty square
+    ]
+)
+def test_first_selection(start_board, sel1, pos1, result):
     select = Select()
+    select.make_selection(sel1, start_board)
 
-    select.make_selection((6, 3), game)
-
-    assert select.pos_1 == (6, 3)
-    assert set(select.moves) == set([(5, 3), (4, 3)])
+    assert select.pos_1 == pos1
+    assert set(select.moves) == set(result)
 
 
-def test_select_first_invalid_black():
-    game = Board()
+def test_first_selection_capture(game):
     select = Select()
-
-    select.make_selection((1, 0), game)
-
-    assert select.pos_1 is None
-    assert select.moves == []
-
-
-def test_select_first_empty():
-    game = Board()
-    select = Select()
-
-    select.make_selection((3, 3), game)
-
-    assert select.pos_1 is None
-    assert select.moves == []
-
-
-# --- First move valid --- #
-
-def test_select_second_empty():
-    game = Board()
-    select = Select()
-
-    select.make_selection((6, 3), game)
-    select.make_selection((3, 3), game)
-
-    assert select.pos_1 is None
-    assert select.moves == []
-
-
-def test_select_second_valid():
-    game = Board()
-    select = Select()
-
-    select.make_selection((6, 3), game)
-    move = select.make_selection((4, 3), game)
-
-    assert select.pos_1 is None
-    assert select.moves == []
-    # assert move == "engine.Move(self.pos_1, select, game).execute()"
-
-
-def test_select_second_same():
-    game = Board()
-    select = Select()
-
-    select.make_selection((6, 3), game)
-    select.make_selection((6, 3), game)
-
-    assert select.pos_1 == (6, 3)
-    assert set(select.moves) == set([(5, 3), (4, 3)])
-
-
-def test_select_second_new():
-    game = Board()
-    select = Select()
-
-    select.make_selection((6, 3), game)
-    select.make_selection((6, 2), game)
-
-    assert select.pos_1 == (6, 2)
-    assert set(select.moves) == set([(5, 2), (4, 2)])
-
-# --- Capture --- #
-
-
-def test_select_precapture_black(game_grid):
-    game = Board(array=game_grid)
-    select = Select()
-
     select.make_selection((4, 3), game)
 
     assert select.pos_1 == (4, 3)
     assert set(select.moves) == set([(3, 3), (3, 4)])
 
 
-def test_select_capture_black(game_grid):
-    game = Board(array=game_grid)
+# --- Second Selection --- # noqa
+@pytest.mark.parametrize(
+    "sel1, sel2, pos1, result", [
+        ((6, 3), (3, 3), None, []),                  # invalid, empty square
+        ((6, 3), (6, 3), (6, 3), [(5, 3), (4, 3)]),  # valid, same square
+        ((6, 3), (6, 2), (6, 2), [(5, 2), (4, 2)])   # valid, new piece
+    ]
+)
+def test_second_selection(start_board, sel1, sel2, pos1, result):
+    select = Select()
+    select.make_selection(sel1, start_board)
+    select.make_selection(sel2, start_board)
+
+    assert select.pos_1 == pos1
+    assert set(select.moves) == set(result)
+
+
+# --- Second Selection (MOVING) --- # noqa
+@patch("chess.engine.Move")
+def test_second_selection_move(mock_Move, start_board):
     select = Select()
 
-    select.make_selection((4, 3), game)
-    move = select.make_selection((3, 4), game)
+    select.make_selection((6, 3), start_board)
+    select.make_selection((4, 3), start_board)
 
     assert select.pos_1 is None
     assert select.moves == []
-    # assert move == "engine.Move(self.pos_1, select, game).execute()"
+    mock_Move.assert_called_with((6, 3), (4, 3), start_board)
+
+
+@patch("chess.engine.Move")
+def test_second_selection_capture(mock_Move, game):
+    select = Select()
+
+    select.make_selection((4, 3), game)
+    select.make_selection((3, 4), game)
+
+    assert select.pos_1 is None
+    assert select.moves == []
+    mock_Move.assert_called_with((4, 3), (3, 4), game)
+
+
+@pytest.fixture
+def game():
+    arr = [
+        ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
+        ["bp", "bp", "bp", "bp", "--", "bp", "bp", "bp"],
+        ["--", "--", "--", "--", "--", "--", "--", "--"],
+        ["--", "--", "--", "--", "bp", "--", "--", "--"],
+        ["--", "--", "--", "wp", "--", "--", "--", "--"],
+        ["--", "--", "--", "--", "--", "--", "--", "--"],
+        ["wp", "wp", "wp", "--", "wp", "wp", "wp", "wp"],
+        ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]
+    ]
+    return Board(array=arr)
